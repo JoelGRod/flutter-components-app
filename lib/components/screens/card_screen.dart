0d8429f1d28b7bf1c6c4f438ee3ea18ecfc7f870
JoelGRod <joelgrod@gmail.com>
2022-02-23 13:12:22 +0000

    cards

--- lib/components/screens/card_screen.dart
+++ lib/components/screens/card_screen.dart
@@ -20,7 +20,21 @@ class CardScreen extends StatelessWidget {
         children: const <Widget>[
           CustomCardTypeOne(),
           SizedBox(height: 10),
-          CustomCardTypeTwo()
+          CustomCardTypeTwo(
+            cardText: 'A Beautiful Landscape I', 
+            imgUrl: 'https://iso.500px.com/wp-content/uploads/2014/06/W4A2827-1-3000x2000.jpg',
+          ),
+          SizedBox(height: 10),
+          CustomCardTypeTwo(
+            cardText: 'A Beautiful Landscape II', 
+            imgUrl: 'https://www.superprof.co.uk/blog/wp-content/uploads/2018/02/landscape-photography-tutorials.jpg',
+          ),
+          SizedBox(height: 10),
+          CustomCardTypeTwo(
+            cardText: 'A Beautiful Landscape III', 
+            imgUrl: 'https://cdn1.epicgames.com/ue/product/Screenshot/1-1920x1080-a58e6c53fee218623cb26ba39786d1e5.jpg?resize=1&w=1920',
+          ),
+          
         ],
       ),
     );
--- lib/components/widgets/custom_card_type_two.dart
+++ lib/components/widgets/custom_card_type_two.dart
@@ -3,7 +3,15 @@ import 'package:flutter/material.dart';
 import 'package:components_app/shared/themes/main_theme.dart';
 
 class CustomCardTypeTwo extends StatelessWidget {
-  const CustomCardTypeTwo({Key? key}) : super(key: key);
+
+  final String imgUrl;
+  final String cardText;
+
+  const CustomCardTypeTwo({
+    Key? key, 
+    required this.imgUrl, 
+    required this.cardText
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,19 +24,19 @@ class CustomCardTypeTwo extends StatelessWidget {
       shadowColor: MainTheme.primaryColor.withOpacity(0.6),
       child: Column(
         children: <Widget>[
-          const FadeInImage(
-            image: NetworkImage('https://thelandscapephotoguy.com/wp-content/uploads/2019/01/landscape%20new%20zealand%20S-shape.jpg'),
-            placeholder: AssetImage('assets/imgs/jar-loading.gif'),
+          FadeInImage(
+            image: NetworkImage(imgUrl),
+            placeholder: const AssetImage('assets/imgs/jar-loading.gif'),
             width: double.infinity,
             height: 230,
             fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 1000),
+            fadeInDuration: const Duration(milliseconds: 1000),
           ),
 
           Container(
             alignment: AlignmentDirectional.center,
             padding: const EdgeInsets.all(15),
-            child: const Text('A beautiful Landscape')
+            child: Text(cardText)
           )
         ],
       ),
