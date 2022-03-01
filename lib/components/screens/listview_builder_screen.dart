dea94c7e82f698b6c49db79b9ac983127e8060b9
joelgrod <joelgrod@gmail.com>
2022-03-01 10:29:21 +0000

    infinite scroll and pull to refresh

--- lib/components/screens/listview_builder_screen.dart
+++ lib/components/screens/listview_builder_screen.dart
@@ -1,3 +1,4 @@
+import 'package:components_app/shared/themes/main_theme.dart';
 import 'package:flutter/material.dart';
 
 class ListViewBuilderScreen extends StatefulWidget {
@@ -32,14 +33,32 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
     isLoading = true;
     setState(() {});
 
+    // HTTP Request Here - Temporal solution
     await Future.delayed(const Duration(seconds: 3));
-
     addFive();
 
     isLoading = false;
     setState(() {});
+
+    if (scrollController.position.pixels + 100 <
+        scrollController.position.maxScrollExtent) return;
+
+    scrollController.animateTo(
+      scrollController.position.pixels + 200,
+      duration: const Duration(seconds: 1), 
+      curve: Curves.fastOutSlowIn
+    );
   }
 
+  Future<void> refreshScreen() async {
+    await Future.delayed( const Duration(seconds: 2) );
+    final int lastId = imgsIds.last + 1;
+    imgsIds.clear();
+    imgsIds.add(lastId);
+    addFive();
+  }
+
+  // Add items Example function
   void addFive() {
     setState(() {
       final lastId = imgsIds.last;
@@ -49,26 +68,65 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: MediaQuery.removePadding(
         context: context,
         removeTop: true,
         removeBottom: true,
-        child: ListView.builder(
-            controller: scrollController,
-            physics: const BouncingScrollPhysics(),
-            itemCount: imgsIds.length,
-            itemBuilder: (BuildContext context, int index) {
-              return FadeInImage(
-                placeholder: const AssetImage('assets/imgs/jar-loading.gif'),
-                image: NetworkImage(
-                    'https://picsum.photos/500/300?image=${imgsIds[index]}'),
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.cover,
-              );
-            }),
+        child: Stack(
+          children: [
+            RefreshIndicator(
+              color: MainTheme.primaryColor,
+              backgroundColor: Colors.white.withOpacity(0.7),
+              onRefresh: refreshScreen,
+              child: ListView.builder(
+                  controller: scrollController,
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: imgsIds.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return FadeInImage(
+                      placeholder:
+                          const AssetImage('assets/imgs/jar-loading.gif'),
+                      image: NetworkImage(
+                          'https://picsum.photos/500/300?image=${imgsIds[index]}'),
+                      width: double.infinity,
+                      height: 300,
+                      fit: BoxFit.cover,
+                    );
+                  }),
+            ),
+            if (isLoading)
+              Positioned(
+                  top: size.height * 0.5 - 50,
+                  left: size.width * 0.5 - 50,
+                  child: const LoadingIcon())
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoadingIcon extends StatelessWidget {
+  const LoadingIcon({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.7),
+        shape: BoxShape.circle,
+      ),
+      child: const CircularProgressIndicator(
+        color: MainTheme.primaryColor,
       ),
     );
   }
